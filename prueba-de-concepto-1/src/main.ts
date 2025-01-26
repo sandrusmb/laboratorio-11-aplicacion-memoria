80481cb98298734a73ec7a952a847f5fd22b4cc0
sandrusmb <sandrusmb@gmail.com>
2025-01-26 18:41:57 +0100

    Create the function to shuffle cards

--- prueba-de-concepto-1/src/main.ts
+++ prueba-de-concepto-1/src/main.ts
@@ -1,3 +1,33 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// Prueba de concepto 1 - Barajar las cartas
+
+const animalsArray: string[] = [
+  "🐭",
+  "🐭",
+  "🦊",
+  "🦊",
+  "🐮",
+  "🐮",
+  "🐷",
+  "🐷",
+  "🐸",
+  "🐸",
+  "🐻",
+  "🐻",
+];
+
+const barajarCartas = (array: string[]) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
+
+barajarCartas(animalsArray);
+
+console.log(barajarCartas(animalsArray));
+console.log(barajarCartas(animalsArray));
+console.log(barajarCartas(animalsArray));
